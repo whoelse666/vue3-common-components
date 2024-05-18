c72ef02868b754096b5d35cf18786b93029645a3
whoelse <347300979@qq.com>
2024-05-18 22:26:43 +0800

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 // 获取组件的类型;
 import { ref } from "vue";
-export function useCompRef<T extends abstract new (...args: any) => any>() {
+export function useCompRef<T extends abstract new (...args: any) => any>(_comp: T) {
   return ref<InstanceType<T>>();
 }
